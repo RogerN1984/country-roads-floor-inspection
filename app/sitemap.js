4b660fc2ab43b7f5fce3c5f2180a3a821289f65d
RogerN1984 <rogerlnestor@gmail.com>
2026-02-18 17:41:31 -0500

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://countryroadsfloorinspection.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://countryroadsfloorinspection.com/baltimore-floor-inspector",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://countryroadsfloorinspection.com/washington-dc-floor-inspector",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://countryroadsfloorinspection.com/winchester-va-floor-inspector",
+      lastModified: new Date(),
+    },
+  ];
+}
